d8366d70465443f787bd480556e60b005368506f
AshamedConsequence <60143430+AshamedConsequence@users.noreply.github.com>
2020-07-13 13:00:37 -0500

    Added owo functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { token } = require('./config.json')
 const bot = new Discord.Client();
 
 const regex = /.*hello\ *there.*/gi;
+const wegex = /.*he(ll|ww)o *the(r|w)e.*/gi; // Makes it so the bot has a specific answer to an uwu'd hello there
 
 bot.on('ready', () => {
 	console.log('Bot is online')
@@ -11,6 +12,8 @@ bot.on('ready', () => {
 bot.on('message', (msg) => {
 	if (msg.content.match(regex)) {
 		msg.reply("you are a bold one")
+	} else if (msg.content.match(wegex)) {
+		msg.reply("you awe a bowd onye")
 	}
 })
 
