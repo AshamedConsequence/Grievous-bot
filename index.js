5f0b0006db80b77195335531c78d24506a73ea83
Maxime Morize <maxime.morize@outlook.com>
2020-07-14 08:49:07 -0500

    tweaks

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 const regex = /.*h *e *l *l *o *t *h *e *r *e.*/gi;
 // Makes it so the bot has a specific answer to an uwu'd hello there
-const wegex = /.*h *e *(l *l|w *w) *o( *|\n*)t *h *e *(r|w) *e.*/gi;
+const wegex = /.*h *e *(l *l|w *w) *o(( |\n)*)t *h *e *(r|w) *e.*/gi;
 
 bot.on('ready', () => {
 	console.log('Bot is online')
@@ -14,7 +14,7 @@ bot.on('message', (msg) => {
 	if (msg.content.match(regex)) {
 		msg.reply("you are a bold one")
 	} else if (msg.content.match(wegex)) {
-		msg.reply("you awe a bowd onye")
+		msg.reply("you awe a bowd wan")
 	}
 })
 
