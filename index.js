da1889c88db9273f12f02e48dba3938752b02f77
Maxime Morize <maxime.morize@outlook.com>
2020-11-30 10:27:04 -0600

    added functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const { token } = require('./config.json')
 const bot = new Discord.Client();
 
+const base = /(hello)|(hewwo)|(hi)|(hey)/gi;
 const regex = /.*h *e *l *l *o(( |\n)*)t *h *e *r *e.*/gi;
 // Makes it so the bot has a specific answer to an uwu'd hello there
 const wegex = /.*h *e *(l *l|w *w) *o(( |\n)*)t *h *e *(r|w) *e.*/gi;
@@ -11,9 +12,12 @@ bot.on('ready', () => {
 })
 
 bot.on('message', (msg) => {
-	if (msg.content.match(regex)) {
+	if (msg.content.match(base) && Math.floor(Math.random() * 11) === 5 ) {
+		msg.reply("say \"hello there\" you uncultured swine");
+	}
+	else if (msg.content.match(regex)&& message.author.id !== client.user.id) {
 		msg.reply("you are a bold one")
-	} else if (msg.content.match(wegex)) {
+	} else if (msg.content.match(wegex)&& message.author.id !== client.user.id) {
 		msg.reply("you awe a bowd wan")
 	}
 })
